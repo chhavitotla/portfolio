7e1e9f4159737611ed36ec6988baef5ddded0f9a
Chhavi <chhavilakshya@gmail.com>
2026-09-18 17:55:29 +0530

    resume fixed

--- src/components/portfolio/IntroSection.tsx
+++ src/components/portfolio/IntroSection.tsx
@@ -92,7 +92,7 @@ export function IntroSection() {
               <Button
                 size="lg"
                 className="bg-gradient-primary hover:shadow-glow-primary transition-all duration-300"
-                onClick={() => window.open('/resume.pdf', '_blank')}
+                onClick={() => window.open('/Chhavi.pdf', '_blank')}
               >
                 <Download className="w-5 h-5 mr-2" />
                 Resume
